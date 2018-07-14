5e368d8aa05b2a24daec44bfdace1ac9ff384864
eCodeing <844958791@qq.com>
2018-07-15 00:07:17 +0800

    #绘制第一个三角形

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -14,6 +14,33 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 	}
 }
 
+const GLchar* vertexShaderSource = "#version 330 core\n"
+	"layout (location = 0) in vec3 position;\n"
+	"void main()\n"
+	"{\n"
+	"gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
+	"}\0";
+const GLchar* fragmentShaderSource = "#version 330 core\n"
+	"out vec4 color;\n"
+	"void main()\n"
+	"{\n"
+	"color = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+	"}\n\0";
+
+void createShaderAndCheckCompile(GLuint shader,const GLchar* shaderSource) {
+	GLint success;
+	GLchar infoLog[512];
+	//附着到shader对象上
+	glShaderSource(shader, 1, &shaderSource, NULL);
+	//编译
+	glCompileShader(shader);
+	//获取编译状态
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		glGetShaderInfoLog(shader, 512, NULL, infoLog);
+		std::cout << "Error::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog;
+	}
+}
 int main(int argc, char* argv[]) {
 
 	std::cout << "Starting GLFW context,OpenGL 3.3" << std::endl;
@@ -47,13 +74,62 @@ int main(int argc, char* argv[]) {
 	glfwSetKeyCallback(window, key_callback);
 
 
+	//创建一个vertexShader
+	GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+	createShaderAndCheckCompile(vertexShader, vertexShaderSource);
+	//创建一个fragmentShader
+	GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	createShaderAndCheckCompile(fragmentShader, fragmentShaderSource);
+	//创建程序对象，并将编译过的shader附加到此对象上
+	GLuint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	//调用
+	glUseProgram(shaderProgram);
+	//干掉不用的资源
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+
+
+	GLfloat vertices[] = {
+		-0.5f,-0.5f,0.0f,
+		0.5f,-0.5f,0.0f,
+		0.0f,0.5f,0.0f
+	};
+	//创建顶点缓冲对象
+	GLuint VBO, VAO;
+	glGenBuffers(1, &VBO);
+	glGenVertexArrays(1, &VAO);
+
+	//顶点缓冲对象的类型是GL_ARRAY_BUFFER
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindVertexArray(VAO);
+	//将定义好的顶点数据通过glBufferData绑定到VBO
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	//链接顶点属性
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat),
+		(GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 	while (!glfwWindowShouldClose(window)) {
 		glfwPollEvents();
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+		//draw first triangle
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glBindVertexArray(0);
 
 		glfwSwapBuffers(window);
 	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
 
 	glfwTerminate();
 
